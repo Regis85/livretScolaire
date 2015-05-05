dd696d3ccf7ef2f47c7fddeb7f39db55cabec9be
Régis Bouguin <regis.bouguin@wanadoo.fr>
2015-05-05 19:28:33 +0200

    passage à des labels + cacher la saisie des compétences lorsqu'elles ne sont pas actives

--- afficheAccueil.php
+++ afficheAccueil.php
@@ -46,7 +46,9 @@ if (!$APBinstalle || 0 == $APBinstalle->num_rows ) {
 	Vous devez avoir effectué les extractions pour APB avant d'utiliser ce plugin
 </p>
 
-<fieldset>
+<fieldset <?php if (!lsl_getDroit('droitCompetences')) { ?> 
+	style="display : none;"
+<?php } ?>	>
 	<legend>Télécharger les définitions des compétences</legend>
 	Chaque ligne du fichier doit contenir 2 colonnes séparées par un point virgule : CODE_COMPETENCE ; LIBELLE_COMPETENCE
 	<br />
@@ -59,45 +61,110 @@ if (!$APBinstalle || 0 == $APBinstalle->num_rows ) {
 			<input type="file" name="fileToUpload" id="fileToUpload">
 			<input type="submit" name="uploadFichier" id="uploadFichier" value="télécharger">	
 		</p>
+	</form>	
 	<?php if ($competences->num_rows) { ?>
-		<p title="Cliquez pour afficher/masquer le tableau" 
-		   onclick="bascule('tableCompetences');"
-		   class="center grand bold"
-			   style="cursor:pointer" >
-			Tableau des compétences
-		</p>
-		<table class="boireaus sortable resizable"
-			   id="tableCompetences"
-			   style="display:none;">
-			<tr>
-				<th>
-					code
-				</th>
-				<th>
-					Compétence
-				</th>
-			</tr>
+	<p title="Cliquez pour afficher/masquer le tableau" 
+	   onclick="bascule('tableCompetences');"
+	   class="center grand bold"
+		   style="cursor:pointer" >
+		Tableau des compétences
+	</p>
+	<table class="boireaus sortable resizable"
+		   id="tableCompetences"
+		   style="display:none;">
+		<tr>
+			<th>
+				code
+			</th>
+			<th>
+				Compétence
+			</th>
+		</tr>
 	<?php 
 		$cpt =1; 
 		while ($obj = $competences->fetch_object()) {
 	?>
 			
-			<tr class="lig<?php echo $cpt; ?>">
-				<td>
-					<?php echo $obj->code_competences; ?>
-				</td>
-				<td>
-					<?php echo $obj->texte_competences; ?>
-				</td>
-			</tr>
+		<tr class="lig<?php echo $cpt; ?>">
+			<td>
+				<?php echo $obj->code_competences; ?>
+			</td>
+			<td id="competences_<?php echo $obj->code_competences; ?>">
+				<?php echo $obj->texte_competences; ?>
+			</td>
+		</tr>
 	<?php
 		$cpt*=-1;
 		}
 	?>
-		</table>
+	</table>
+
+
+	<p title="Cliquez pour afficher/masquer le tableau" 
+	   onclick="bascule('tableCorrespondances');"
+	   class="center grand bold"
+		   style="cursor:pointer" >
+		Compétences par MEF
+	</p>
+	<form method="post" action="index.php" id="form_LSL_correspondances" enctype="multipart/form-data" >
+		<p class="center">
+			<?php if (function_exists("add_token_field")) echo add_token_field(); ?>
+			Nouvelle association MEF ←→ compétence
+		</p>
+		<p>
+			MEF <input type="text" name="lsl_mef" style="width:7em;" />
+			←→ 
+			<input type="text" 
+				   name="lsl_code" 
+				   id="lsl_code_competence" 
+				   style="width:4em;" 
+				   onblur="afficheCompetences(getElementById('lsl_code_competence').value);"/> 
+			Code compétence 
+		</p>
+		<p>
+			Modalité <input type="text" name="lsl_modalite" style="width:2em;"/>
+			Matière <input type="text" name="lsl_matiere" style="width:9em;"/>
+			Note obligatoire <input type="text" name="lsl_note" style="width:2em;"/>
+			Appréciation obligatoire <input type="text" name="lsl_appreciation" style="width:2em;"/>
+		</p>
+		<p>
+			<input type="submit" name="creeCorrespondance" id="creeCorrespondance" value="Créer">				
+		</p>
+	</form>
+		
+	<table class="boireaus sortable resizable"
+		   id="tableCorrespondances"
+		   style="display:none;">
+		<tr>
+			<th>
+				MEF
+			</th>
+			<th>
+				Regroupement
+			</th>
+			<th>
+				Code compétence
+			</th>
+			<th>
+				Modalité
+			</th>
+			<th>
+				Descriptif
+			</th>
+			<th>
+				Matière
+			</th>
+			<th>
+				Note obligatoire
+			</th>
+			<th>
+				Appréciation obligatoire
+			</th>
+		</tr>
+	</table>
+		
 	<?php } ?>
 		
-	</form>	
 </fieldset>
 
 <fieldset>
@@ -193,19 +260,24 @@ if (!$APBinstalle || 0 == $APBinstalle->num_rows ) {
 	<legend>Modules ouverts</legend>
 	<form method="post" action="index.php" id="form_LSL_ouvert" enctype="multipart/form-data">
 		<p>
-			Saisie des appréciations
+			<label for="droitAppreciation">
+				Saisie des appréciations
+			</label>
 			<input type="checkbox" 
 				   name="droitAppreciation" 
 				   <?php if (lsl_getDroit('droitAppreciation')) {echo " checked='checked' ";} ?>
 					  />
 		</p>
 		<p>
-			Saisie des compétences
+			<label for="droitCompetences">
+				Saisie des compétences
+			</label>
 			<input type="checkbox" 
 				   name="droitCompetences" 
 				   <?php if (lsl_getDroit('droitCompetences')) {echo " checked='checked' ";} ?>
 				   disabled="disabled"
 					  />
+			<span style='color:red'>non encore implémenté...</span>
 		</p>
 		<p>
 			<?php if (function_exists("add_token_field")) echo add_token_field(); ?>
